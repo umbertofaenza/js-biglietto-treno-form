a7b90c8007680abe962248417d19790642233a13
umbertofaenza <umberto.faenza@gmail.com>
2023-07-20 17:30:37 +0200

    feat: add target for other info

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-const targetPrice = document.getElementById("target-price");
-
-const targetName = document.getElementById("target-name");
-
 const userInputName = document.getElementById("user-name");
-
 const userInputKm = document.getElementById("user-km");
-
 const userInputAge = document.getElementById("user-age");
-
 const sendBtn = document.getElementById("send-btn");
+
+const targetPrice = document.getElementById("target-price");
+const targetName = document.getElementById("target-name");
+const targetTicketType = document.getElementById("target-ticket-type");
+const targetCoachNumber = document.getElementById("target-coach-number");
+const targetCodeNumber = document.getElementById("target-code-number");
 
 sendBtn.addEventListener("click", function () {
   const userKm = parseFloat(userInputKm.value);
@@ -25,6 +24,9 @@ sendBtn.addEventListener("click", function () {
   const finalPrice = basePrice - ticketDiscount;
   targetPrice.innerHTML = "€" + finalPrice.toFixed(2);
   targetName.innerHTML = userInputName.value;
+  targetTicketType.innerHTML = "Biglietto Standard";
+  targetCoachNumber.innerHTML = "5";
+  targetCodeNumber.innerHTML = "92911";
 
   userInputName.value = "";
   userInputKm.value = "";
